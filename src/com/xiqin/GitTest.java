f532b872a55fd9975146ec0baafb6c81206cad23
xiqin-huang <13676299537@163.com>
2022-10-14 14:58:07 +0800

    main commit2

--- src/com/xiqin/GitTest.java
+++ src/com/xiqin/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,world!");
         System.out.println("hello,git3!");
         System.out.println("hello,git4hot-fix!");
+        System.out.println("main test!");
     }
 }
