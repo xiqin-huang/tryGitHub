1329c0981eae47111079a6ade8d36f263d7ad39f
xiqin-huang <13676299537@163.com>
2022-10-14 15:45:10 +0800

    GitHub push

--- src/com/xiqin/GitTest.java
+++ src/com/xiqin/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello,git4hot-fix!");
         System.out.println("main test!");
         System.out.println("main test!");
+        System.out.println("push test!");
     }
 }
